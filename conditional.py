868e83b630a827a94136c4022f76d673376ea55b
Devesh Sharma <deveshsharma1721@gmail.com>
2023-06-02 18:55:12 +0530

    Update conditional.py

--- conditional.py
+++ conditional.py
@@ -1,15 +1,1 @@
-hrs =input("hours")
-rate= input("rate")
-h= float(hrs)
-rate=float(rate)
-r=h*rate
-if h<40 :
-    print("not eligible", r)
-elif h==40:
-    print("ok will see what can be done",r)
-else:
-    print('theek hai bhaai jeet gayi india iss baar')
-    p=h-40
-    p=p*1.5*rate
-    p=(40*rate)+p
-    print(p)
+print ("this is a test python")
